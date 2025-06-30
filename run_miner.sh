0d2780034de9bd71411e97ee94389b8ab5443715
min1933 <153935134+min1933@users.noreply.github.com>
2025-07-01 05:57:28 +0700

    Delete run_miner.sh

--- run_miner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# For more usage info: hellminer --help
-
-# Example SSL URL
-# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
-
-./hellminer stratum+tcp://sg.vipor.net:5040 -u ADDRESS.WORKER -p x --cpu 1
